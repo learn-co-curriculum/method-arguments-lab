35c19a26efb359894a92f6d25404de34026a182d
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-10-28 11:30:40 -0400

    lab done with solution

---
+++ lib/introduction.rb
@@ -0,0 +1,8 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
