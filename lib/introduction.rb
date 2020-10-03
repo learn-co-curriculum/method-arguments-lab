6eb957eb7ae5b26eaee8bfd1bb2c1e2defbd2b6e
jmsimmons90 <kutsuukun@gmail.com>
2020-10-03 11:44:00 -0700

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,11 @@
+#lib/introduction.rb
+
+def introduction(name)
+    puts "Hi, my name is #{name}."
+end
+
+#lib/introduction.rb
+
+def introduction_with_language(name,language)
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
